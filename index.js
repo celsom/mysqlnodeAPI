59aae151c8cb6c64af0a54345d25ea6341b7d97c
celsom <celsomanhique97@gmail.com>
2018-11-22 21:42:50 +0200

    test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var http = require("http");
+const cors = require('cors');
 var express = require('express');
 var app = express();
 var mysql      = require('mysql');
@@ -25,6 +26,9 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 //end body-parser configuration
 
+app.use(cors());
+app.options('*', cors());
+
 //create app server
 var server = app.listen(3000,  "127.0.0.1", function () {
 
@@ -43,36 +47,73 @@ app.get('/customer', function (req, res) {
 	});
 });
 //rest api to get a single customer data
-app.get('/customer/:id', function (req, res) {
-   connection.query('select * from customers where Id=?', [req.params.id], function (error, results, fields) {
-	  if (error) throw error;
-	  res.end(JSON.stringify(results));
-	});
+// app.get('/customer/:id', function (req, res) {
+//    connection.query('select * from customers where Id=?', [req.params.id], function (error, results, fields) {
+// 	  if (error) throw error;
+// 	  res.end(JSON.stringify(results));
+// 	});
+// });
+
+
+app.get('/customer/:Id', (request, response) => {
+    const Id = request.params.Id;
+ 
+    connection.query('SELECT * FROM customer WHERE Id = ?', Id, (error, result) => {
+        if (error) throw error;
+ 
+        response.send(result);
+    });
 });
+
+
 
 //rest api to create a new customer record into mysql database
 app.post('/customer', function (req, res) {
-   var params  = req.body;
+   var params  = req.customer;
+   var body  = req;
+   console.log(body);
    console.log(params);
-   connection.query('INSERT INTO customer SET ?', params, function (error, results, fields) {
-	  if (error) throw error;
-	  res.end(JSON.stringify(results));
-	});
+   //   connection.query('INSERT INTO customer SET ?', params, function (error, results, fields) {
+  	//   if (error) throw error;
+  	//   res.end(JSON.stringify(results));
+  	// });
 });
 
 //rest api to update record into mysql database
-app.put('/customer', function (req, res) {
-   connection.query('UPDATE `customer` SET `Name`=?,`Address`=?,`Country`=?,`Phone`=? where `Id`=?', [req.body.Name,req.body.Address, req.body.Country, req.body.Phone, req.body.Id], function (error, results, fields) {
-	  if (error) throw error;
-	  res.end(JSON.stringify(results));
-	});
+// app.put('/customer/:Id', (req, res) =>{
+      
+//    connection.query('UPDATE `customer` SET `Name`=?,`Address`=?,`Country`=?,`Phone`=? where `Id`=?', [req.body.Name,req.body.Address, req.body.Country, req.body.Phone, req.body.Id], function (error, results, fields) {
+// 	  if (error) throw error;
+// 	  response.send('User updated successfully.');
+// 	});
+// });
+
+app.put('/customer/:Id', (request, response) => {
+    const Id = request.params.Id;
+ 
+    connection.query('UPDATE customer SET ? WHERE Id = ?', [request.body, Id], (error, result) => {
+        if (error) throw error;
+ 
+        response.send('User updated successfully.');
+    });
 });
 
+
 //rest api to delete record from mysql database
-app.delete('/customer', function (req, res) {
-   console.log(req.body);
-   connection.query('DELETE FROM `customer` WHERE `Id`=?', [req.body.Id], function (error, results, fields) {
-	  if (error) throw error;
-	  res.end('Record has been deleted!');
-	});
+// app.delete('/customer', function (req, res) {
+//    console.log(req.body);
+//    connection.query('DELETE FROM `customer` WHERE `Id`=?', [req.body.Id], function (error, results, fields) {
+// 	  if (error) throw error;
+// 	  res.end('Record has been deleted!');
+// 	});
+// });
+
+app.delete('/customer/:Id', (request, response) => {
+    const Id = request.params.Id;
+ 
+    connection.query('DELETE FROM customer WHERE Id = ?', Id, (error, result) => {
+        if (error) throw error;
+ 
+        response.send('User deleted.');
+    });
 });
